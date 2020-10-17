fc82d027c77c0821344dbf2b8db21f5bd511cfb1
Reza <reza.bozorgi@outlook.com>
2020-10-17 21:48:10 +0330

    commit

---
+++ build/precache-manifest.463718c34ef0fa1b4a719b22095622b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b3e5269b7acd4d561e758fb81cc40a3",
+    "url": "/jokes-generaotor/index.html"
+  },
+  {
+    "revision": "61a51f726e9641f645bf",
+    "url": "/jokes-generaotor/static/css/main.d81dc2b7.chunk.css"
+  },
+  {
+    "revision": "d6259fb06fd5e3ccc67b",
+    "url": "/jokes-generaotor/static/js/2.5f70c731.chunk.js"
+  },
+  {
+    "revision": "f406aedd96f61bd83ab19677f9afa536",
+    "url": "/jokes-generaotor/static/js/2.5f70c731.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61a51f726e9641f645bf",
+    "url": "/jokes-generaotor/static/js/main.6e41bb44.chunk.js"
+  },
+  {
+    "revision": "0dab3afc73bd60127f3a",
+    "url": "/jokes-generaotor/static/js/runtime-main.6495e24e.js"
+  }
+]);
