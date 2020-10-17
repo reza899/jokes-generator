5dbd66c207013c4f1fb115bc1d9a9e9b84cd0fec
Reza <reza.bozorgi@outlook.com>
2020-10-17 21:49:53 +0330

    baseurl changed

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={process.env.homepage}>
       <App />
     </Router>
   </React.StrictMode>,
