e5228700eff641bb5a0dd58605d2bebadfa17ac0
Reza <reza.bozorgi@outlook.com>
2020-10-21 00:29:10 +0330

    fix basename

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
             <li>
               <Link
                 to={{
-                  pathname: "category",
+                  pathname: "/category",
                 }}
               >
                 Categories
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename={process.env.homepage}>
+    <Router basename="/jokes-generator/">
       <App />
     </Router>
   </React.StrictMode>,
